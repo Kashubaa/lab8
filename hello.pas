156a6c8cc0cdf5d5918350e8171fa02b39784872
Екатерина Кашуба <ekashuba@sfedu.ru>
2023-09-30 11:18:25 +0300

    5

--- hello.pas
+++ hello.pas
@@ -1,3 +1,7 @@
-﻿begin
-  print('Привет, мир!');
+﻿﻿begin
+ var c := readinteger('какой час ?');
+ case c of
+ 4..10: println('доброе утро, мир!');
+ 11..16: println('добрый вечер, мир!');
+ 17..22: println('доброй ночи, мир!');
 end.
